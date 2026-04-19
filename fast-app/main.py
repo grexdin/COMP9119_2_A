a469bf7c57f7bb3e905bb7418568e472b2536aea
Georgi Mirchev <grexdin@protonmail.com>
2026-04-19 17:42:53 +0100

    small change

--- fast-app/main.py
+++ fast-app/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from pathlib import Path
 import math
 
-app = FastAPI(title="RunCalc Pro")
+app = FastAPI(title="RunCalc super Pro")
 
 BASE_DIR = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
